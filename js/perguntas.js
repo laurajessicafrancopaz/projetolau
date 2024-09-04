aca342aee20288db0ab4e3bacf7fda2a626f28ee
laurajessicafrancopaz <132076966+laurajessicafrancopaz@users.noreply.github.com>
2024-09-04 11:17:10 -0300

    Add files via upload

---
+++ js/perguntas.js
@@ -0,0 +1,96 @@
+export const perguntas = [
+    {
+        enunciado: "você faz o uso do insta?",
+        alternativas: [
+            {
+                texto: "sim, eu amooo",
+                afirmacao: [
+                    "que bom, eu também uso o instagram.",
+                ],
+                proxima: 1,
+            },
+            {
+                texto: "não uso",
+                afirmacao: [
+                    "Se vc não usa, sinto muito.",
+                ],
+                proxima: 2,
+            },
+        ]
+    },
+    {
+        enunciado: "o que vc acha do instagram?.",
+        alternativas: [
+            {
+                texto: "uma rede social de muito aprendizado",
+                afirmacao: [
+                    "eu concordo. Também aprendo muito la.",
+                ],
+            },
+            {
+                texto: "perca de tempo",
+                afirmacao: [
+                    "que pena, eu não acho perca de tempo."
+                ],
+            },
+        ]
+    },
+    {
+        enunciado: "em janeiro de 2025, o instagram vai retirar os filtros criados por influêncers. você está de acordo com isso?",
+        alternativas: [
+            {
+                texto: "óbvio, vai perder a graça",
+                afirmacao: [
+                    "sim! os filtros são a graça do insta né?.",
+                ],
+                proxima: 3,
+            },
+            {
+                texto: "tanto faz, eu não uso",
+                afirmacao: [
+                    "isso é bom!'",
+                ],
+            },
+        ]
+    },
+    {
+        enunciado: "o que vc me diz sobre os termos de privacidade do insta?",
+        alternativas: [
+            {
+                texto: "eu acho que são seguros",
+                afirmacao: [
+                    "são seguros mesmo, ja foi comprovado.",
+                ],
+                proxima: 4,
+            },
+            {
+                texto: "aceito, mas não confio",
+                afirmacao: [
+                    "ok!",
+                ],
+            },
+        ]
+    },
+    {
+        enunciado: "obrigada por responder o meu quizz",
+        alternativas: [
+            {
+                texto: "Denada Laura linda!",
+                afirmacao: [
+                ],
+                proxima: 5,
+            },
+    
+        ]
+    },
+    {
+        enunciado: "você me chamou de linda? obrigada !!!",
+        alternativas: [
+            {
+                texto: "Claro! você é mesmo..",
+                afirmacao: [
+                ],
+            }
+        ]
+    },
+];
